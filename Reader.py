fc71848eea78576d75e292ea2c17e3cd420337d7
alicechiozza <122796621+alicechiozza@users.noreply.github.com>
2023-01-16 14:20:13 +0100

    Update Reader.py

--- Reader.py
+++ Reader.py
@@ -2,24 +2,31 @@ from Dataset import *
 from abc import ABC, abstractmethod
 import pandas as pd
 
-
-# abstract class for reading the file
 class DatasetReader(ABC):
-    '''def __init__(self, filename): #a reader in general receives a file as input
-        self.filename = filename''' 
-    # ^ lo abbiamo messo come commento per provare a rendere DatasetReader una abstract INTERFACE e non abstract class; lo reimplementiamo in Gff3Reader
-
+    '''
+    Abstract interface 
+    '''
     @abstractmethod
-    def read(self): #abstract reading method
+    def read(self): 
+        '''
+        Abstract reading method
+        '''
         pass
 
-
-# implementation of the reader from gff3 files to pandas dataframes returning a Dataset
-class Gff3Reader(DatasetReader): #subclass of the abstract reading class
-    def __init__(self, filename): #a reader in general receives a file as input
+class Gff3Reader(DatasetReader): 
+    '''
+    Subclass of the abstract reading class specific for Gff3 files
+    '''
+    def __init__(self, filename): 
+        '''
+        A reader reiceives a file as input  
+        '''
         self.filename = filename
         
     def read(self):
+        '''
+        Overriding of the abstract method read(): given a Gff3 file, it returns a Dataset object
+        '''
         df = pd.read_csv(self.filename, delimiter="\t", comment="#",names=["seqID", "source", "type", "start", "end", "score", "strand", "phase", "attributes"], na_values='.') #with na_values we are substituting every '.' in the dataframe with NaN
         return Dataset(df)
 
