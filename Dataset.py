2a03cb311ce79a4d0a7fc761301f81a0f8abafff
Anna <annacormio01@gmail.com>
2023-01-16 15:04:05 +0100

    comments on methods

--- Dataset.py
+++ Dataset.py
@@ -13,7 +13,6 @@ class Dataset:
         """
          Retrieves the private Pandas DataFrame attribute
         """
-
         return self.__df
 
     #@active decorator is called over each function 
@@ -22,14 +21,14 @@ class Dataset:
         '''
         Returns a Dataset object with the column names and the data type they contain
         '''
-        dfBasic = (self.getDf().dtypes.reset_index())
+        dfBasic = (self.getDf().dtypes.reset_index()) # reset_index returns a DF with two columns and not a Series
         return Dataset(dfBasic.rename(columns={"index": "column_name", 0: "data type"}))
 
     def __uniqueList(self, column):  
         '''
         Private method that given the column label returns a Pandas DataFrame with the column's unique values
         '''
-        dfUnique = self.getDf()[column].unique()
+        dfUnique = self.getDf()[column].unique() #retrive the column of interest from the dataframe and delete all duplicates using unique()
         return pd.DataFrame(data=dfUnique, columns=[column])
 
     @active
@@ -46,13 +45,11 @@ class Dataset:
         '''
         return Dataset(self.__uniqueList(column='type').sort_values(by='type', ignore_index=True))
 
-    def __uniqueCount(self,
-                      column):  
+    def __uniqueCount(self,column):
         '''
         Private method that given the column label returns a Pandas DataFrame with the count of each one of its values
         '''
-        uniqueDf = self.getDf().groupby(column, dropna=False).size().sort_values(
-            ascending=False).reset_index()  # reset_index returns a DF with two columns
+        uniqueDf = self.getDf().groupby(column, dropna=False).size().sort_values(ascending=False).reset_index()  # reset_index returns a DF with two columns
         return uniqueDf.rename(columns={0: "count"})
 
     @active
@@ -70,8 +67,7 @@ class Dataset:
         return Dataset(self.__uniqueCount(column='type'))
 
     @active
-    def entireChromosome(
-            self):  
+    def entireChromosome(self):
         '''
         Returns a Dataset object selecting from the column source only the rows having GRCh38 as source
         '''
@@ -79,23 +75,19 @@ class Dataset:
         # we drop the source column from the DataFrame because it will provide only GRCh38 sources since that was the filtering parameter
 
     @active
-    def unassembledSequence(
-            self):  
+    def unassembledSequence(self):
         '''
         Returns a Dataset object with the fraction and percentage of unassembled sequences over entire chromosomes set
         '''
         entire = self.entireChromosome().getDf()  # retrieves the Dataframe of only GRCh38 source
-        unassembled = entire[
-            entire['type'] == 'supercontig']  # selects from these rows only the ones of type supercontig
-        fraction = str(unassembled.shape[0]) + "/" + str(entire.shape[0])  # fraction
-        perc = round(float(unassembled.shape[0]) * 100 / float(entire.shape[0]),
-                     2)  # percentage rounded on 2 decimal
+        unassembled = entire[entire['type'] == 'supercontig']  # selects from these rows only the ones of type supercontig
+        fraction = str(unassembled.shape[0]) + "/" + str(entire.shape[0])  # fraction of supercontig content over all dataframe content
+        perc = round(float(unassembled.shape[0]) * 100 / float(entire.shape[0]),2)  # percentage rounded on 2 decimal
         values = [fraction, perc]
         return Dataset(pd.DataFrame([values], columns=["fraction", "percentage %"]))
 
     @active
-    def onlyEnsemblHavana(
-            self):  
+    def onlyEnsemblHavana(self):
         '''
         Returns a Dataset object selecting only the rows having ensembl, havana and ensembl_havana as source
         '''
@@ -109,7 +101,7 @@ class Dataset:
         Returns a Dataset object with the count of the types of sequences in the ensembl, havana and ensembl_havana Dataset
         '''
         filteredDs = self.onlyEnsemblHavana()  
-        return filteredDs.countType()
+        return filteredDs.countType() #using the method instantiated before for counting the Type in a Dataset
 
     @active
     def ensemblHavanaGenes(self):  
@@ -127,10 +119,10 @@ class Dataset:
             # we create an attribute dictionary for code readability, populating it one attribute by one
             attDict = {}
             for i in attList:
-                (key, value) = i.split("=") # we divide the elements of the attList by "="
+                (key, value) = i.split("=") # we split the elements of the attList by "="
                 attDict[key] = value # we add the elements in the dictionary
 
-            geneNames.append(attDict['Name']) # we add the gene name in the genNames list 
+            geneNames.append(attDict['Name']) # we add the gene name in the geneNames list
 
         # we convert the geneName list to set to delete duplicates 
         # set does not allow duplicates so all genes names repeated, if there are any, are dropped
