2d85e7db315d0a6d9224ab5db8771583d05a3114
alicechiozza <122796621+alicechiozza@users.noreply.github.com>
2023-01-16 14:23:24 +0100

    Update Decorator.py

--- Decorator.py
+++ Decorator.py
@@ -1,15 +1,21 @@
 import pandas as pd
-#implementing the decorator
+
 def active(func):
-    #read the .csv file containing the functions activity or not
+    '''
+    Decorator that reads the .csv file containing information on the functions' activities
+    '''
     dfAct =pd.read_csv('settings/activeFunctions.csv') #returns a dataframe
     def wrapper(*args):
+        '''
+        If the function is active, the function is executed
+        If the function is not active, an Exception is raised
+        '''
         funcName = func.__name__ #get just the name of the function
         #filtering rows of the data
         funcrow = dfAct[dfAct['Function'] == funcName]
         # if the function exists, funcrow has only 1 row
         if (funcrow.shape[0] == 1) and (funcrow.iloc[0]['Active'] == True):# here I check that it actually is just a row and that the 'Active' status is True
-            return func(*args) #if condition is satisfied I execute the function
+            return func(*args) 
         else:
             raise Exception("The function is not active or it does not exist")
 
